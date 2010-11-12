12c179e91a8fe7a55f08c38813a3fdea296abf1d
Colin Kuebler <kueblc@rpi.edu>
2010-11-12 11:56:14 +0000

    Moved the rule pane north.
    git-svn-id: https://subversion.hss.rpi.edu/winsvn/legup/trunk@268 361cd0e3-9505-6840-87d6-36ed75c01346

--- code/edu/rpi/phil/legup/newgui/LEGUP_Gui.java
+++ code/edu/rpi/phil/legup/newgui/LEGUP_Gui.java
@@ -325,7 +325,7 @@ public class LEGUP_Gui extends JFrame implements ActionListener, TreeSelectionLi
 		treeBox.add( tree, BorderLayout.EAST );
 		
 		justificationFrame = new JustificationFrame( this );
-		ruleBox.add( justificationFrame, BorderLayout.WEST );
+		ruleBox.add( justificationFrame, BorderLayout.NORTH );
 		
 		board = new Board( this );
 		board.setPreferredSize( new Dimension( 600, 400 ) );
