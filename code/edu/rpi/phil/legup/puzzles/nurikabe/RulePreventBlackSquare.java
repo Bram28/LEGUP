b890400a71901f4a06cec878d370de546c096ab2
Mitchell Mellone <mellom3@rpi.edu>
2015-02-11 16:47:39 -0500

    Changed PreventBlackSquare rule to use contradictions.

--- code/edu/rpi/phil/legup/puzzles/nurikabe/RulePreventBlackSquare.java
+++ code/edu/rpi/phil/legup/puzzles/nurikabe/RulePreventBlackSquare.java
@@ -2,8 +2,16 @@ package edu.rpi.phil.legup.puzzles.nurikabe;
 
 import javax.swing.ImageIcon;
 
+import java.awt.Point;
+import java.util.Set;
+import java.util.LinkedHashSet;
+
+import javax.swing.ImageIcon;
+
 import edu.rpi.phil.legup.BoardState;
 import edu.rpi.phil.legup.PuzzleRule;
+import edu.rpi.phil.legup.Contradiction;
+import edu.rpi.phil.legup.puzzles.nurikabe.ContradictionNoNumber;
 
 public class RulePreventBlackSquare extends PuzzleRule
 {
@@ -27,135 +35,37 @@ public class RulePreventBlackSquare extends PuzzleRule
      * @param state The board state
      * @return null if the contradiction was applied correctly, the error String otherwise
      */
-	 protected String checkRuleRaw(BoardState destBoardState)
-    {
-    	String error = null;
-    	boolean changed = false;
-    	BoardState origBoardState = destBoardState.getSingleParentState();
+	protected String checkRuleRaw(BoardState destBoardState)
+  {
+		Set<Contradiction> contras = new LinkedHashSet<Contradiction>();
+		contras.add(new ContradictionBlackSquare());
 
-    	// Check for only one branch
+		BoardState origBoardState = destBoardState.getSingleParentState();
+		int width = origBoardState.getWidth();
+		int height = origBoardState.getHeight();
+
+		// Check for only one branch
 		if (destBoardState.getParents().size() != 1)
 		{
-			error = "This rule only involves having a single branch!";
+			return "This rule only involves having a single branch!";
 		}
-		else
-		{
 
-			for (int y = 0; y < origBoardState.getHeight() && error == null; ++y)
-			{
-				for (int x = 0; x < origBoardState.getWidth(); ++x)
-				{
-					int origState = origBoardState.getCellContents(x,y);
-					int newState = destBoardState.getCellContents(x,y);
-
-					if (origState != newState)
-					{
-						changed = true;
-
-						if (newState != Nurikabe.CELL_WHITE || origState != 0)
-						{
-							error = "This rule only involves adding white cells!";
-							break;
-						}
-
-						if(!checkPreventsSquare(destBoardState, x,y, destBoardState.getWidth(), destBoardState.getHeight()))
-						{
-							error = "White cell must be placed within a 2x2 box which contains 3 black cells.";
-							break;
-						}
-
-
-
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				if (destBoardState.getCellContents(x, y) !=
+						origBoardState.getCellContents(x, y)) {
+					if (destBoardState.getCellContents(x, y) != Nurikabe.CELL_WHITE) {
+						return "Only white cells are allowed for this rule!";
+					}
+					BoardState modified = origBoardState.copy();
+					modified.getBoardCells()[y][x] = Nurikabe.CELL_BLACK;
+					for (Contradiction c : contras) {
+						if (c.checkContradictionRaw(modified) != null)
+							return "White cell must be placed within a 2x2 box which contains 3 black cells.";
 					}
 				}
 			}
-
-			if (error == null && !changed)
-			{
-				error = "You must add a white cell to use this rule!";
-			}
 		}
-
-		return error;
-    }
-
-	 protected boolean doDefaultApplicationRaw(BoardState destBoardState)
-	  {
-		 BoardState origBoardState = destBoardState.getSingleParentState();
-	    	boolean changed = false;
-	    	int width = destBoardState.getWidth();
-	    	int height = destBoardState.getHeight();
-
-
-	    	if (origBoardState != null && destBoardState.getParents().size() == 1)
-	    	{
-	        	for(int x = 0; x < width - 1; ++x)
-	        	{
-	        		for(int y = 0; y < height - 1; ++y)
-	        		{
-	        			if(destBoardState.getCellContents(x,y) == Nurikabe.CELL_BLACK && destBoardState.getCellContents(x + 1,y) == Nurikabe.CELL_BLACK && destBoardState.getCellContents(x,y + 1) == Nurikabe.CELL_BLACK)
-	        			{
-	        				if(destBoardState.getCellContents(x+1,y+1)>0)
-	        					continue;
-	        				destBoardState.setCellContents(x + 1,y + 1, Nurikabe.CELL_WHITE);
-	        			}
-	        			else if(destBoardState.getCellContents(x,y) == Nurikabe.CELL_BLACK && destBoardState.getCellContents(x + 1,y) == Nurikabe.CELL_BLACK && destBoardState.getCellContents(x+1,y + 1) == Nurikabe.CELL_BLACK)
-	        			{
-	        				if(destBoardState.getCellContents(x,y+1)>0)
-	        					continue;
-	        				destBoardState.setCellContents(x,y + 1, Nurikabe.CELL_WHITE);
-	        			}
-	        			else if(destBoardState.getCellContents(x,y) == Nurikabe.CELL_BLACK && destBoardState.getCellContents(x + 1,y + 1) == Nurikabe.CELL_BLACK && destBoardState.getCellContents(x,y + 1) == Nurikabe.CELL_BLACK)
-	        			{
-	        				if(destBoardState.getCellContents(x+1,y)>0)
-	        					continue;
-	        				destBoardState.setCellContents(x + 1,y, Nurikabe.CELL_WHITE);
-	        			}
-	        			else if(destBoardState.getCellContents(x+1,y+1) == Nurikabe.CELL_BLACK && destBoardState.getCellContents(x + 1,y) == Nurikabe.CELL_BLACK && destBoardState.getCellContents(x,y + 1) == Nurikabe.CELL_BLACK)
-	        			{
-	        				if(destBoardState.getCellContents(x,y)>0)
-	        					continue;
-	        				destBoardState.setCellContents(x,y, Nurikabe.CELL_WHITE);
-	        			}
-	        		}
-	        	}
-		    	String error = checkRuleRaw(destBoardState);
-
-				if (error == null)
-				{
-					changed = true;
-					// valid change
-				}
-	    	}
-
-	    	if(!changed)
-	    	{
-	    		destBoardState = origBoardState.copy();
-	    	}
-
-		    return changed;
-	  }
-
-
-	 private boolean checkPreventsSquare(BoardState state, int x, int y, int width, int height)
-	 {
-		 boolean ret = false;
-
-		 if(x > 0)
-		 {
-			 if(y > 0)
-				 ret = ret | (state.getCellContents(x - 1,y) == Nurikabe.CELL_BLACK && state.getCellContents(x - 1,y - 1) == Nurikabe.CELL_BLACK && state.getCellContents(x,y - 1) == Nurikabe.CELL_BLACK);
-			 if(y < height - 1)
-				 ret = ret | (state.getCellContents(x - 1,y) == Nurikabe.CELL_BLACK && state.getCellContents(x - 1,y + 1) == Nurikabe.CELL_BLACK && state.getCellContents(x,y + 1) == Nurikabe.CELL_BLACK);
-		 }
-		 if(x < width - 1)
-		 {
-			 if(y > 0)
-				 ret = ret | (state.getCellContents(x + 1,y) == Nurikabe.CELL_BLACK && state.getCellContents(x + 1,y - 1) == Nurikabe.CELL_BLACK && state.getCellContents(x,y - 1) == Nurikabe.CELL_BLACK);
-			 if(y < height - 1)
-				 ret = ret | (state.getCellContents(x + 1,y) == Nurikabe.CELL_BLACK && state.getCellContents(x + 1,y + 1) == Nurikabe.CELL_BLACK && state.getCellContents(x,y + 1) == Nurikabe.CELL_BLACK);
-		 }
-
-		 return ret;
-	 }
+		return null;
+	}
 }
