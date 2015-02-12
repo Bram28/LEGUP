c7ea41fc58ffa568228d8349f15153f34137abfd
Avi Weinstock <aweinstock314@gmail.com>
2015-02-12 13:49:58 -0500

    Added description of getPositionsForPointSet.

--- code/edu/rpi/phil/legup/PointSetAlgorithms.java
+++ code/edu/rpi/phil/legup/PointSetAlgorithms.java
@@ -22,6 +22,27 @@ public class PointSetAlgorithms
         // AWT Rectangles are point-size (not point-point)
         return new Rectangle(minX, minY, maxX - minX, maxY - minY);
     }
+    /*
+    PointSetAlgorithms.getPositionsForPointSet is intended to be used for BattleShip (at least), but is written here in a generic way.
+    To find the positions a 4x1 battleship can possibly be in, this would be called with
+    {(0,0), (1,0), (2,0), (3,0)} as the "inner" argument (the coordinates of the shape of the ship, at the origin), and
+    {(-1,-1),(0,-1),(1,-1),(2,-1),(3,-1),(4,-1),
+     (-1, 0),                            (4, 0),
+     (-1, 1),(0, 1),(1, 1),(2, 1),(3, 1),(4, 1)} as the "outer" argument (the coordinates of what needs to be "water", since ships can't be adjacent).
+    innerMask and outerMask specify what is allowed to be already present in the grid.
+    Generic unknown (PuzzleModule.CELL_UNKNOWN) is not assumed to be valid, and so should be included explicitly into the masks.
+    In the case of BattleShip, innerMask should be all the different types of ship segments (and generic unknown). outerMask should be just water and generic unknown.
+    outer will be allowed to be outside the grid (i.e. with the above shapes, (0, 0) may be part of the return set)
+    
+    The return value is a set of translations to be applied to "inner" and "outer"
+    */
+    public static Set<Point> getPositionsForPointSet(int[][] grid,
+                                                    Set<Point> inner, Set<Point> outer,
+                                                    Set<Integer> innerMask, Set<Integer> outerMask)
+    {
+        // TODO: implement once description is approved
+        return null;
+    }
 }
 
 /*
