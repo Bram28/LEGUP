6c69333491f62cad836e5035630094b370f60bb1
Avi Weinstock <aweinstock314@gmail.com>
2015-02-17 14:58:50 -0500

    Implemented PointSetAlgorithms.getPositionsForPointSet, with modifications discussed with Aaron. Includes documentation and tests.

--- code/edu/rpi/phil/legup/PointSetAlgorithms.java
+++ code/edu/rpi/phil/legup/PointSetAlgorithms.java
@@ -2,6 +2,8 @@ package edu.rpi.phil.legup;
 
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class PointSetAlgorithms
@@ -22,27 +24,90 @@ public class PointSetAlgorithms
         // AWT Rectangles are point-size (not point-point)
         return new Rectangle(minX, minY, maxX - minX, maxY - minY);
     }
+    public static Set<Point> translatePointSet(Set<Point> pointSet, int dx, int dy)
+    {
+        Set<Point> result = new LinkedHashSet<Point>();
+        for(Point p : pointSet) { result.add(new Point(p.x+dx, p.y+dy)); }
+        return result;
+    }
     /*
     PointSetAlgorithms.getPositionsForPointSet is intended to be used for BattleShip (at least), but is written here in a generic way.
     To find the positions a 4x1 battleship can possibly be in, this would be called with
-    {(0,0), (1,0), (2,0), (3,0)} as the "inner" argument (the coordinates of the shape of the ship, at the origin), and
-    {(-1,-1),(0,-1),(1,-1),(2,-1),(3,-1),(4,-1),
-     (-1, 0),                            (4, 0),
-     (-1, 1),(0, 1),(1, 1),(2, 1),(3, 1),(4, 1)} as the "outer" argument (the coordinates of what needs to be "water", since ships can't be adjacent).
-    innerMask and outerMask specify what is allowed to be already present in the grid.
+    (using the notation {KEY -> VALUE, ...} for maps, and (x,y) for points)
+    {(-1,-1) -> O,(0,-1) -> O,(1,-1) -> O,(2,-1) -> O,(3,-1) -> O,(4,-1) -> O,
+     (-1, 0) -> O,(0, 0) -> L,(1, 0) -> M,(2, 0) -> M,(3, 0) -> R,(4, 0) -> O,
+     (-1, 1) -> O,(0, 1) -> O,(1, 1) -> O,(2, 1) -> O,(3, 1) -> O,(4, 1) -> O}
+    where O, L, M, and R are the sets
+    O = {POINT_OUTSIDE, CELL_UNKNOWN, CELL_WATER}
+    L = {CELL_UNKNOWN, CELL_SHIP, CELL_LEFT_CAP}
+    M = {CELL_UNKNOWN, CELL_SHIP, CELL_MIDDLE}
+    R = {CELL_UNKNOWN, CELL_SHIP, CELL_RIGHT_CAP}
+    (all the CELL_ constants are from the BattleShip PuzzleModule, and POINT_OUTSIDE is a constant on PointSetAlgorithms)
+
+    The "masks" argument specifies what is allowed to be already present in the grid.
+    The magic value POINT_OUTSIDE in a mask specifies that a point is allowed the be outside the grid.
     Generic unknown (PuzzleModule.CELL_UNKNOWN) is not assumed to be valid, and so should be included explicitly into the masks.
-    In the case of BattleShip, innerMask should be all the different types of ship segments (and generic unknown). outerMask should be just water and generic unknown.
-    outer will be allowed to be outside the grid (i.e. with the above shapes, (0, 0) may be part of the return set)
-    
-    The return value is a set of translations to be applied to "inner" and "outer"
+    The return value is a set of translations
     */
-    public static Set<Point> getPositionsForPointSet(int[][] grid,
-                                                    Set<Point> inner, Set<Point> outer,
-                                                    Set<Integer> innerMask, Set<Integer> outerMask)
+    public static final Integer POINT_OUTSIDE = -42;
+    public static Set<Point> getPositionsForPointSet(int[][] grid, Map<Point, Set<Integer>> masks)
     {
-        // TODO: implement once description is approved
-        return null;
+        Set<Point> results = new LinkedHashSet<Point>();
+        int height = grid.length;
+        // assumes grid isn't actually a jagged array
+        // TODO: consider a different internal representation for 
+        //  the backing arrays, that would eliminate these sorts of 
+        //   edge cases?
+        int width = grid[0].length;
+        for(int dy = 0; dy < height; dy++) {
+            for(int dx = 0; dx < width; dx++) {
+                if(gPFPS_validateTranslation(grid, masks, dx, dy)) {
+                    results.add(new Point(dx, dy));
+                }
+            }
+        }
+        return results;
     }
+    private static boolean gPFPS_validateTranslation(int[][] grid, Map<Point, Set<Integer>> masks, int dx, int dy)
+    {
+        Set<Point> pointSet = masks.keySet();
+        //Set<Point> translated = translatePointSet(masks.keySet(), dx, dy);
+        int height = grid.length;
+        int width = grid[0].length;
+        for(Point p : pointSet) {
+            Point t = new Point(p.x+dx, p.y+dy);
+            if((t.x < 0) || (t.x >= width) || (t.y < 0) || (t.y >= height)) {
+                if(masks.get(p).contains(POINT_OUTSIDE)) { continue; }
+                else { return false; }
+            }
+            if(!masks.get(p).contains(grid[t.y][t.x])) { return false; }
+        }
+        return true;
+    }
+    /*
+    ; Test for getPositionsForPointSet, in Kawa
+    (let ((PSA edu.rpi.phil.legup.PointSetAlgorithms) (B edu.rpi.phil.legup.puzzles.battleship.BattleShip))
+        (define grid (int[][]
+            (int[] B:CELL_UNKNOWN B:CELL_UNKNOWN B:CELL_UNKNOWN B:CELL_BOTTOM_CAP)
+            (int[] B:CELL_UNKNOWN B:CELL_UNKNOWN B:CELL_UNKNOWN B:CELL_UNKNOWN)
+            (int[] B:CELL_UNKNOWN B:CELL_SHIP B:CELL_UNKNOWN B:CELL_UNKNOWN)
+            (int[] B:CELL_UNKNOWN B:CELL_WATER B:CELL_UNKNOWN B:CELL_UNKNOWN)
+            (int[] B:CELL_UNKNOWN B:CELL_UNKNOWN B:CELL_SHIP B:CELL_UNKNOWN)
+        ))
+        (define mask4x1ship (java.util.LinkedHashMap))
+        (define (make-set . items) (let ((set (java.util.LinkedHashSet))) (for-each set:add items) set))
+        (define outs (make-set PSA:POINT_OUTSIDE B:CELL_UNKNOWN B:CELL_WATER))
+        (define left (make-set B:CELL_UNKNOWN B:CELL_SHIP B:CELL_LEFT_CAP))
+        (define midl (make-set B:CELL_UNKNOWN B:CELL_SHIP B:CELL_MIDDLE))
+        (define rght (make-set B:CELL_UNKNOWN B:CELL_SHIP B:CELL_RIGHT_CAP))
+        (for-each (lambda (x y set) (mask4x1ship:put (java.awt.Point x y) set))
+      '(-1    0    1    2    3    4   -1    0    1    2    3    4   -1    0    1    2    3    4)
+      '(-1   -1   -1   -1   -1   -1    0    0    0    0    0    0    1    1    1    1    1    1)
+(list outs outs outs outs outs outs outs left midl midl rght outs outs outs outs outs outs outs)
+        )
+        (PSA:getPositionsForPointSet grid mask4x1ship)
+    )
+    */
 }
 
 /*
