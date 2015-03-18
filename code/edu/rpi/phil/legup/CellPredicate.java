62619152c479ce18319bee43513b0713f3e1fca7
Avi Weinstock <aweinstock314@gmail.com>
2015-03-18 17:09:24 -0400

    Added CellPredicate class (had forgotten to commit it previously).

---
+++ code/edu/rpi/phil/legup/CellPredicate.java
@@ -0,0 +1,46 @@
+package edu.rpi.phil.legup;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public abstract class CellPredicate
+{
+    public abstract boolean check(BoardState s, int x, int y);
+
+    public static boolean inBounds(BoardState s, int x, int y, boolean includeEdges) {
+        int w = s.getWidth(); int h = s.getHeight();
+        if(includeEdges) { return !((x < -1)||(x > w)||(y < -1)||(y > h)); }
+        else { return !((x <= -1)||(x >= w)||(y <= -1)||(y >= h)); }
+    }
+    public static boolean isCorner(BoardState s, int x, int y) {
+        int w = s.getWidth(); int h = s.getHeight();
+        return ((x == -1) && (y == -1)) ||
+               ((x ==  w) && (y == -1)) ||
+               ((x == -1) && (y ==  h)) ||
+               ((x ==  w) && (y ==  h));
+    }
+    public static CellPredicate modifiableCell() {
+        return new CellPredicate() { @Override public boolean check(BoardState s, int x, int y) {
+            return inBounds(s, x, y, false) && s.isModifiableCell(x, y);
+        }};
+    }
+    public static CellPredicate columnRowLabel() {
+        return new CellPredicate() { @Override public boolean check(BoardState s, int x, int y) {
+            return inBounds(s, x, y, true) && !inBounds(s, x, y, false) && !isCorner(s, x, y);
+        }};
+    }
+    public static CellPredicate typeWhitelist(final Integer... whitelist) {
+        return typeWhitelist(new LinkedHashSet<Integer>(Arrays.asList(whitelist)));
+    }
+    public static CellPredicate typeWhitelist(final Set<Integer> whitelist) {
+        return new CellPredicate() { @Override public boolean check(BoardState s, int x, int y) {
+            return inBounds(s, x, y, false) && whitelist.contains(s.getCellContents(x, y));
+        }};
+    }
+    public static CellPredicate constFalse() {
+        return new CellPredicate() { @Override public boolean check(BoardState s, int x, int y) {
+            return false;
+        }};
+    }
+}
