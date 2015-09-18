28420bc456d24b09014433d44306110ce8a28f99
Mitchell Mellone <mellom3@rpi.edu>
2015-09-18 00:38:41 -0400

    Re-wrote ruleFinishWithEmpty() to use case/contradiction algorithm

--- code/edu/rpi/phil/legup/puzzles/lightup/RuleFinishWithEmpty.java
+++ code/edu/rpi/phil/legup/puzzles/lightup/RuleFinishWithEmpty.java
@@ -1,252 +1,69 @@
 package edu.rpi.phil.legup.puzzles.lightup;
 
-import javax.swing.ImageIcon;
+import java.util.Set;
+import java.util.LinkedHashSet;
 
 import edu.rpi.phil.legup.BoardState;
 import edu.rpi.phil.legup.PuzzleRule;
+import edu.rpi.phil.legup.Contradiction;
 
 public class RuleFinishWithEmpty extends PuzzleRule
 {
 	static final long serialVersionUID = 2828176895339413023L;
 	public String getImageName() {return "images/lightup/rules/SurroundEmpty.png";}
-	 RuleFinishWithEmpty()
-	 {
+	RuleFinishWithEmpty()
+	{
 		setName("Finish with Empty");
 		description = "The remaining unknowns around a block must be empty if the number is satisfied.";
-		//image = new ImageIcon("images/lightup/rules/SurroundWhite.png");
-	 }
+	}
 
-	 /**
-     * Checks if the contradiction was applied correctly to this board state
-     *
-     * @param state The board state
-     * @return null if the contradiction was applied correctly, the error String otherwise
-     */
-	 protected String checkRuleRaw(BoardState destBoardState)
-    {
-    	String error = null;
-    	boolean changed = false;
-    	BoardState origBoardState = destBoardState.getSingleParentState();
-    	int width = destBoardState.getWidth();
-    	int height = destBoardState.getHeight();
-    	boolean foundvalidblock;
-
-    	// Check for only one branch
-		if (destBoardState.getParents().size() != 1)
-		{
-			error = "This rule only involves having a single branch!";
+	/**
+	* Checks if RuleFinishWithEmpty was correctly applied to this board state.
+	* If declaring a cell adjacent to a number to be a bulb causes a contradiction
+	* where too many light bulbs can be placed to satisfy the number the rule
+	* was applied correctly. (i.e. the number of light bulb cells adjacent to number
+	* equals the number).
+	*
+	* @param state The board state
+	* @return null if the contradiction was applied correctly, the error String otherwise
+	*/
+	protected String checkRuleRaw(BoardState destBoardState) {
+		// Check for only one branch
+		if (destBoardState.getParents().size() != 1) {
+			return "This rule only involves having a single branch!";
 		}
-		else
-		{
-			for (int y = 0; y < origBoardState.getHeight() && error == null; ++y)
-			{
-				for (int x = 0; x < origBoardState.getWidth(); ++x)
-				{
-					int origState = origBoardState.getCellContents(x,y);
-					int newState = destBoardState.getCellContents(x,y);
 
-					if (origState != newState)
-					{
-						changed = true;
+		// Add contradictions to check to set contras
+		Set<Contradiction> contras = new LinkedHashSet<Contradiction>();
+		contras.add(new ContradictionTooManyBulbs());
 
-						if (newState != LightUp.CELL_EMPTY || origState != LightUp.CELL_UNKNOWN)
-						{
-							error = "This rule only involves adding white cells!";
-							break;
-						}
+		// Copy the parent state to compare with current state to find changes
+		BoardState origBoardState = destBoardState.getSingleParentState();
+		int width = origBoardState.getWidth();
+		int height = origBoardState.getHeight();
 
-						foundvalidblock = false;
-						if(x > 0)
-							if(destBoardState.getCellContents(x-1,y)>= 10 && destBoardState.getCellContents(x-1,y)< 15)
-								foundvalidblock = foundvalidblock | checkBlockCompleted(destBoardState,x-1,y,width,height);
-						if(x < width - 1)
-							if(destBoardState.getCellContents(x+1,y)>= 10 && destBoardState.getCellContents(x+1,y)< 15)
-								foundvalidblock = foundvalidblock | checkBlockCompleted(destBoardState,x+1,y,width,height);
-						if(y > 0)
-							if(destBoardState.getCellContents(x,y-1)>= 10 && destBoardState.getCellContents(x,y-1)< 15)
-								foundvalidblock = foundvalidblock | checkBlockCompleted(destBoardState,x,y-1,width,height);
-						if(y < height - 1)
-							if(destBoardState.getCellContents(x,y+1)>= 10 && destBoardState.getCellContents(x,y+1)< 15)
-								foundvalidblock = foundvalidblock | checkBlockCompleted(destBoardState,x,y+1,width,height);
+		// Check each tile for any changes,
+		// make sure changes are allowed
+		// check that doing the opposite case would lead to a contradiction (thus proving the rule)
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				// Check for changes
+				if (destBoardState.getCellContents(x, y) != origBoardState.getCellContents(x, y)) {
+					//Make sure cells placed are empty cells
+					if (destBoardState.getCellContents(x, y) != LightUp.CELL_EMPTY) {
+						return "Only empty cells are allowed for this rule!";
+					}
 
-						if(!foundvalidblock)
-						{
-							error = "A white cell must be placed to complete a block's number.";
-							break;
-						}
-
-
-
+					// Create alternative boardstate to apply other case/contradiction
+					BoardState modified = origBoardState.copy();
+					modified.getBoardCells()[y][x] = LightUp.CELL_LIGHT;
+					for (Contradiction c : contras) {
+						if (c.checkContradictionRaw(modified) != null)
+							return "It is not required for the modified cell(s) to be empty!";
 					}
 				}
 			}
-
-			if (error == null && !changed)
-			{
-				error = "You must add a white cell to use this rule!";
-			}
 		}
-
-		return error;
-    }
-
-	 protected boolean doDefaultApplicationRaw(BoardState destBoardState)
-	  {
-		 BoardState origBoardState = destBoardState.getSingleParentState();
-	    	boolean changed = false;
-	    	int width = destBoardState.getWidth();
-	    	int height = destBoardState.getHeight();
-	    	int cellvalue = 0;
-
-	    	if (origBoardState != null && destBoardState.getParents().size() == 1)
-	    	{
-	    		for (int y = 0; y < origBoardState.getHeight(); ++y)
-				{
-					for (int x = 0; x < origBoardState.getWidth(); ++x)
-					{
-						cellvalue = destBoardState.getCellContents(x,y);
-						if(cellvalue >= 10 && cellvalue < 15)
-						{
-							if(checkBlockCompletable(destBoardState,x,y,width,height))
-							{
-								completeBlock(destBoardState,x,y,width,height);
-							}
-						}
-					}
-				}
-
-		    	String error = checkRuleRaw(destBoardState);
-
-				if (error == null)
-				{
-					changed = true;
-					// valid change
-				}
-	    	}
-
-	    	if(!changed)
-	    	{
-	    		destBoardState = origBoardState.copy();
-	    	}
-
-		    return changed;
-	  }
-
-	 private boolean checkBlockCompleted(BoardState destBoardState, int x, int y, int width, int height)
-	 {
-
-			int bulbs = 0;
-			int blanks = 0;
-			if(x > 0)
-			{
-				if(destBoardState.getCellContents(x-1, y) == LightUp.CELL_LIGHT)
-					++bulbs;
-				if(destBoardState.getCellContents(x-1, y) == LightUp.CELL_EMPTY|| destBoardState.getCellContents(x-1, y) >= 10)
-					++blanks;
-			}
-			else
-				++blanks;
-			if(x < width - 1)
-			{
-				if(destBoardState.getCellContents(x+1, y) == LightUp.CELL_LIGHT)
-					++bulbs;
-				if(destBoardState.getCellContents(x+1, y) == LightUp.CELL_EMPTY|| destBoardState.getCellContents(x+1, y) >= 10)
-					++blanks;
-			}
-			else
-				++blanks;
-			if(y > 0)
-			{
-				if(destBoardState.getCellContents(x, y-1) == LightUp.CELL_LIGHT)
-					++bulbs;
-				if(destBoardState.getCellContents(x, y-1) == LightUp.CELL_EMPTY|| destBoardState.getCellContents(x, y-1) >= 10)
-					++blanks;
-			}
-			else
-				++blanks;
-			if(y < height - 1)
-			{
-				if(destBoardState.getCellContents(x, y+1) == LightUp.CELL_LIGHT)
-					++bulbs;
-				if(destBoardState.getCellContents(x, y+1) == LightUp.CELL_EMPTY|| destBoardState.getCellContents(x, y+1) >= 10)
-					++blanks;
-			}
-			else
-				++blanks;
-
-
-		 return ((bulbs + blanks <= 4) && (bulbs == destBoardState.getCellContents(x, y)-10));
-	 }
-
-	 private boolean checkBlockCompletable(BoardState destBoardState, int x, int y, int width, int height)
-	 {
-
-			int bulbs = 0;
-			int blanks = 0;
-			if(x > 0)
-			{
-				if(destBoardState.getCellContents(x-1, y) == LightUp.CELL_LIGHT)
-					++bulbs;
-				if(destBoardState.getCellContents(x-1, y) == LightUp.CELL_EMPTY || destBoardState.getCellContents(x-1, y) >= 10)
-					++blanks;
-			}
-			else
-				++blanks;
-			if(x < width - 1)
-			{
-				if(destBoardState.getCellContents(x+1, y) == LightUp.CELL_LIGHT)
-					++bulbs;
-				if(destBoardState.getCellContents(x+1, y) == LightUp.CELL_EMPTY|| destBoardState.getCellContents(x+1, y) >= 10)
-					++blanks;
-			}
-			else
-				++blanks;
-			if(y > 0)
-			{
-				if(destBoardState.getCellContents(x, y-1) == LightUp.CELL_LIGHT)
-					++bulbs;
-				if(destBoardState.getCellContents(x, y-1) == LightUp.CELL_EMPTY|| destBoardState.getCellContents(x, y-1) >= 10)
-					++blanks;
-			}
-			else
-				++blanks;
-			if(y < height - 1)
-			{
-				if(destBoardState.getCellContents(x, y+1) == LightUp.CELL_LIGHT)
-					++bulbs;
-				if(destBoardState.getCellContents(x, y+1) == LightUp.CELL_EMPTY|| destBoardState.getCellContents(x, y+1) >= 10)
-					++blanks;
-			}
-			else
-				++blanks;
-
-
-		 return (4 - blanks - bulbs == 4 - (destBoardState.getCellContents(x, y)-10) - blanks);
-	 }
-
-	 private void completeBlock(BoardState destBoardState, int x, int y, int width, int height)
-	 {
-
-			if(x > 0)
-			{
-				if(destBoardState.getCellContents(x-1, y) == LightUp.CELL_UNKNOWN)
-					destBoardState.setCellContents(x-1, y, LightUp.CELL_EMPTY);
-			}
-			if(x < width - 1)
-			{
-				if(destBoardState.getCellContents(x+1, y) == LightUp.CELL_UNKNOWN)
-					destBoardState.setCellContents(x+1, y, LightUp.CELL_EMPTY);
-			}
-			if(y > 0)
-			{
-				if(destBoardState.getCellContents(x, y-1) == LightUp.CELL_UNKNOWN)
-					destBoardState.setCellContents(x, y-1, LightUp.CELL_EMPTY);
-			}
-			if(y < height - 1)
-			{
-				if(destBoardState.getCellContents(x, y+1) == LightUp.CELL_UNKNOWN)
-					destBoardState.setCellContents(x, y+1, LightUp.CELL_EMPTY);
-			}
-	 }
-
+		return null;
+	}
 }
