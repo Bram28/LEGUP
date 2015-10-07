464d2076639ac3f10a171886a7e08c9918006cb4
Aaron Perl <perla@rpi.edu>
2015-10-07 16:21:14 -0400

    Fixed null pointer exception on clicking a cell in the initial state

--- code/edu/rpi/phil/legup/newgui/NormalBoard.java
+++ code/edu/rpi/phil/legup/newgui/NormalBoard.java
@@ -161,7 +161,7 @@ public class NormalBoard extends Board
 					lastMousePoint = new Point(p);
 					if (p.x < w && p.y < h)
 					{ // p.x and p.y hold the grid point now!
-						if (state.isModifiableCell(p.x,p.y) || pm.isRemodifiable(state.getSingleParentState().getCellContents(p.x, p.y)))
+						if (state.isModifiableCell(p.x,p.y) || (state.getSingleParentState() != null && pm.isRemodifiable(state.getSingleParentState().getCellContents(p.x, p.y))))
 						{
 							BoardState next = state.conditionalAddTransition();
 							if(next != null) { pm.mousePressedEvent(next,p); }
