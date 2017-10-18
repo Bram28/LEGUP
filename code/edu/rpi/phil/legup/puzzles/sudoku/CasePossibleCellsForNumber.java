5d3a7a8a3312e260392e070a0132500bb7d280cd
Chandrika Sinha <sinhac@rpi.edu>
2017-10-17 22:05:36 -0400

    Updated fillapix files based on new analysis.

--- code/edu/rpi/phil/legup/puzzles/sudoku/CasePossibleCellsForNumber.java
+++ code/edu/rpi/phil/legup/puzzles/sudoku/CasePossibleCellsForNumber.java
@@ -21,6 +21,7 @@ public class CasePossibleCellsForNumber extends CaseRule
 	private static final long serialVersionUID = 174002227L;
 
 	private int currNumSelected = -1;
+	private int numCases;
 
 	public CasePossibleCellsForNumber()
 	{
@@ -64,6 +65,7 @@ public class CasePossibleCellsForNumber extends CaseRule
 
   public BoardState autoGenerateCases(BoardState cur, Point pointSelected)
 	{
+		numCases = 0;
 		Contradiction contra = new ContradictionRepeatedNumber();
 		int xLo, xHi, yLo, yHi;
 		if (pointSelected.x < 0) {
@@ -100,10 +102,15 @@ public class CasePossibleCellsForNumber extends CaseRule
 				tmp.setCaseSplitJustification(this);
 				tmp.setCellContents(x, y, num);
 				tmp.endTransition();
+				numCases+=1;
 			}
 		}
 
 		return Legup.getCurrentState();
+	}
+
+	public boolean isSingleCase() {
+		return numCases==1;
 	}
 
 	public String checkCaseRuleRaw(BoardState state)
