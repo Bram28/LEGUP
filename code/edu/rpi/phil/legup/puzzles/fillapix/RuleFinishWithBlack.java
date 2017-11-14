ff8fee808c114895a3948539674e4a8e438137ce
Chandrika Sinha <sinhac@rpi.edu>
2017-11-14 01:00:37 -0500

    Fixed issue of still having modifiable cells after cell has been proven. Removed unnecessary @param statements.

--- code/edu/rpi/phil/legup/puzzles/fillapix/RuleFinishWithBlack.java
+++ code/edu/rpi/phil/legup/puzzles/fillapix/RuleFinishWithBlack.java
@@ -1,5 +1,6 @@
 package edu.rpi.phil.legup.puzzles.fillapix;
 
+import java.util.ArrayList;
 import java.util.Set;
 import java.util.LinkedHashSet;
 
@@ -51,7 +52,7 @@ public class RuleFinishWithBlack extends PuzzleRule
 			for (int x = 0; x < width; x++) {
 				// Check for changes
 				if (destBoardState.getCellContents(x, y) != origBoardState.getCellContents(x, y)) {
-					//Make sure cells placed are black cells
+					// Make sure cells placed are black cells
 					if (!Fillapix.isBlack(destBoardState.getCellContents(x, y))) {
 						return "Only black cells are allowed for this rule!";
 					}
@@ -71,87 +72,5 @@ public class RuleFinishWithBlack extends PuzzleRule
 			}
 		}
 		return null;
-
-
-    	/* OLD SYSTEM?
-    	String error = null;
-    	BoardState origBoardState = destBoardState.getSingleParentState();
-
-		boolean anychange = false;
-		// Check for only one branch
-		if (destBoardState.getParents().size() != 1) {
-			error = "This rule only involves having a single branch!";
-		} else {
-			int[][] forceMatrix = generateForceMatrix(origBoardState);
-	    	outer: for (int y = 0; y < destBoardState.getHeight(); ++y)
-	    	{
-	    		for (int x = 0; x < destBoardState.getWidth(); ++x)
-	    		{
-	    			int o = origBoardState.getCellContents(x,y);
-					int n = destBoardState.getCellContents(x,y);
-
-					if (o != n)
-					{
-						anychange = true;
-						if (o != Fillapix.CELL_UNKNOWN)
-						{
-							error = "You can not change pixels, only define them.";
-							break outer;
-						}
-						else if (n != forceMatrix[y][x])
-						{
-							error = "The cell at (" + x + ", " + y + ") is not forced.";
-							break outer;
-						}
-					}
-	    		}
-	    	}
-
-			if (!anychange)
-				error = "You must define a pixel to apply this rule!";
-		}
-		return error;
-		*/
 	}
-
-	/*
-	protected int[][] generateForceMatrix(BoardState state)
-	{
-		int w = state.getWidth(), h = state.getHeight();
-
-		int[][] mat = new int[h][w];
-
-		for (int y = 0; y < h; y++) for (int x = 0; x < w; x++) mat[y][x] = state.getCellContents(x, y);
-		for (ExtraCellNumber ECN : Fillapix.getECNs(state))
-		{
-			for (Point p : ECN.getForcedFills(state)) mat[p.y][p.x] = Fillapix.FILLED;
-			for (Point p : ECN.getForcedWhite(state)) mat[p.y][p.x] = Fillapix.EMPTY;
-		}
-
-		return mat;
-	}
-
-	protected boolean doDefaultApplicationRaw(BoardState state)
-	{
-		boolean updated = false;
-		BoardState noUpdate = state.getSingleParentState().copy();
-
-		int[][] forceMatrix = generateForceMatrix(state);
-
-		int w = state.getWidth(), h = state.getHeight();
-		for (int y = 0; y < h; y++) for (int x = 0; x < w; x++)
-			if (state.getCellContents(x, y) != forceMatrix[y][x])
-			{
-				updated = true;
-				state.setCellContents(x, y, forceMatrix[y][x]);
-			}
-
-		if (!updated)
-			state = noUpdate;
-
-    	Legup.getInstance().getPuzzleModule().updateState(state);
-
-		return updated;
-	}
-	*/
 }
